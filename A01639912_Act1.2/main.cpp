8d90d8ec24b908fb015e0bf320aa79e3a222c11c
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2022-08-30 21:10:47 -0500

    Initia dynamic approach

--- A01639912_Act1.2/main.cpp
+++ A01639912_Act1.2/main.cpp
@@ -14,12 +14,35 @@ vector<int> greedyCoins(vector<int> coins,int remaining_P){
     return result;
 }
 
-vector<int> dynamicCoins(){
-    
+vector<int> dynamicCoins(vector<int> coins,int remaining_P){
+    vector<int> dynamic (remaining_P+1,-1);
+    vector<int> used (remaining_P+1,-1);
+    vector<int> results(coins.size(),0);
+
+    dynamic[0] = 0; 
+    used[0] = 0;
+
+    for (int i = 1; i < remaining_P+1;i++){
+        
+        int j = 0;
+        for (int coin: coins){
+            if(i - coin >= -1){
+                if(dynamic[i] == -1 || dynamic[i] > dynamic[i-coin]+1){ 
+                    dynamic[i] = dynamic[i-coin]+1;
+                    used[i] = coin;
+                    results[j] += 1;
+                }
+            }
+            j++;
+        }
+    }
+    cout<<dynamic[remaining_P];
+    return results;
 }
 
 void printVector(vector<int> &coins,vector<int> &res){
-    cout<<"-----------------------------------\n";
+    int total = 0;
+    cout<<"-----------------------------------\n\n";
     for(int i = 0; i < coins.size(); i++){
         cout<<coins[i]<<"\t";
     }
@@ -27,7 +50,9 @@ void printVector(vector<int> &coins,vector<int> &res){
 
     for(int i = 0; i < coins.size(); i++){
         cout<<res[i]<<"\t";
+        total += res[i];
     }
+    cout<<"\n\nModenas utilizadas: "<<total<<"\n";
     cout<<"\n-----------------------------------\n";
 
 }
@@ -44,7 +69,7 @@ int main(){
     
     for (int i = 0; i < N; i++){
         int temp;
-        cout<<"coins[0]: ";
+        cout<<"coins["<<i<<"]:";
         cin>>temp;
 
         coins.push_back(temp);
@@ -59,11 +84,14 @@ int main(){
     cout<<"Pago:";
     cin>>Q;
 
-    int remaining_P = P-Q;
+    int remaining_P = Q-P;
 
     grCoins = greedyCoins(coins,remaining_P);
-    
+    dyCoins = dynamicCoins(coins,remaining_P);
+    cout<<"\nAlgoritmo Avaro:\n";
     printVector(coins,grCoins);
+    cout<<"\nProgramación Dinámica:\n";
+    printVector(coins,dyCoins);
 
     return 0;
 }
