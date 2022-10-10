ff4aba8c3075462202ab2e16f4063dab0c0605d8
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2022-10-10 00:04:20 -0500

    main functionality

--- A01639912_Act3.1/main.cpp
+++ A01639912_Act3.1/main.cpp
@@ -1,17 +1,43 @@
 #include "trie.h"
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main(){
     Trie trie;
-    trie.insert("hoja");
-    trie.insert("hola");
-    trie.insert("computadora");
-    trie.insert("computar");
-    trie.DFS();
-    cout<<trie.searchWord("Hoja")<<endl;
-    cout<<trie.searchWord("compu")<<endl;
+    int N = 0;
+    int M = 0;
+    vector<string> searchWords;
+    
+    cout<<"-----------------------------------\n";
+    cin>>N;
+
+    for(int i = 0; i < N; i++){
+        string temp;
+        cout<<i+1<<": ";
+        cin>>temp;
+        trie.insert(temp);
+    }
+    cout<<"-----------------------------------\n";
+
+    cin>>M;
+
+    for(int i = 0; i < M; i++){
+        string temp;
+        cout<<i+1<<": ";
+        cin>>temp;
+        searchWords.push_back(temp);
+    }
+    cout<<"\nRecorrido DFS----------------------\n";
+    trie.DFS();    
+    cout<<"-----------------------------------\n";
+    
+    for(int i = 0; i < M; i++){
+        string result = trie.searchWord(searchWords[i]) ? "True" : "False";
+        cout<<searchWords[i]<<": "<<result<<"\n";
+    }
+
+    cout<<"-----------------------------------\n";
 
     return 0;
 }
