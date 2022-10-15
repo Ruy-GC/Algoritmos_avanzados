81c1ed1e8eeba250676a3baeebeda300f985808a
David López <davidlopezsth@gmail.com>
2022-10-14 20:52:03 -0500

    added Floyd

--- A01639912_Act3.2/graph.h
+++ A01639912_Act3.2/graph.h
@@ -2,65 +2,124 @@
 #include <iostream>
 #include <queue>
 
+#define INF 10000
+
 using namespace std;
 
+class Graph
+{
+public:
+    vector<vector<int>> matrizAdj;
+    int vertices;
 
-class Graph{
-    public:
-        vector<vector<int>> matrizAdj;
-        int vertices;
+    Graph(vector<vector<int>> matrizAdj)
+    {
+        this->matrizAdj = matrizAdj;
+        this->vertices = matrizAdj.size();
+    }
 
-        Graph(vector<vector<int>> matrizAdj){
-            this->matrizAdj= matrizAdj;
-            this->vertices = matrizAdj.size();
-        }
+    void printSolution(vector<int> results)
+    {
+        cout << "Vertex \t Distance from Source" << endl;
+        for (int i = 0; i < this->matrizAdj.size(); i++)
+            cout << i << " \t\t\t\t" << results[i] << endl;
+    }
 
-        void printSolution(vector<int> results){
-            cout << "Vertex \t Distance from Source" << endl;
-            for (int i = 0; i < this->matrizAdj.size(); i++)
-                cout << i << " \t\t\t\t" << results[i] << endl;
-        }
+    void dijkstra(int root)
+    {
+        vector<int> results(this->matrizAdj.size(), 1000);
+        vector<bool> visited(this->matrizAdj.size(), false);
+        results[root] = 0;
+        priority_queue<pair<int, int>> pq;
 
-        void dijkstra(int root){
-            vector<int> results(this->matrizAdj.size(),1000);
-            vector<bool> visited(this->matrizAdj.size(),false);
-            results[root] = 0;
-            priority_queue<pair<int,int>> pq;
-            
-            pq.push(make_pair(root,0));
+        pq.push(make_pair(root, 0));
 
-            while(pq.size() != 0){
-                int index = pq.top().first;
-                int minVal = pq.top().second;
-                pq.pop();
-                visited[index] = true;
+        while (pq.size() != 0)
+        {
+            int index = pq.top().first;
+            int minVal = pq.top().second;
+            pq.pop();
+            visited[index] = true;
 
-                for(int i = 0; i < this->vertices; i++){
-                    if(visited[i] == true)
-                        continue;
-                    
-                    int newDist = results[index] + this->matrizAdj[root][i];
+            for (int i = 0; i < this->vertices; i++)
+            {
+                if (visited[i] == true)
+                    continue;
 
-                    if (newDist > -1){
-                        if (newDist < results[i]){
-                            results[i] = newDist;
-                            pq.push(make_pair(i,newDist));
-                        }
+                int newDist = results[index] + this->matrizAdj[root][i];
+
+                if (newDist > -1)
+                {
+                    if (newDist < results[i])
+                    {
+                        results[i] = newDist;
+                        pq.push(make_pair(i, newDist));
                     }
-                    
-
                 }
             }
-
-            printSolution(results);    
-
         }
 
+        printSolution(results);
+    }
 
+    // Función que calcula la distanica entre todos los nodos.
+    // Complejidad temporal: O(n^3)
+    // Complejidad espacial: O(n^2)
+    void floyd(int noVertex)
+    {
 
-        void floyd(){
+        vector<vector<int>> provMatrix(noVertex, vector<int>(noVertex));
 
+        for (int i = 0; i < this->matrizAdj.size(); i++)
+        {
+            for (int j = 0; j < this->matrizAdj[i].size(); j++)
+            {
+                if (this->matrizAdj[i][j] == -1)
+                {
+                    provMatrix[i][j] = INF;
+                }
+                else
+                {
+                    provMatrix[i][j] = this->matrizAdj[i][j];
+                }
+            }
         }
+
+        for (int k = 0; k < noVertex; k++)
+        {
+            for (int i = 0; i < noVertex; i++)
+            {
+                for (int j = 0; j < noVertex; j++)
+                {
+                    if (provMatrix[k][j] + provMatrix[i][k] < provMatrix[i][j])
+                    {
+                        provMatrix[i][j] = provMatrix[k][j] + provMatrix[i][k];
+                    }
+                }
+            }
+        }
+        printFloyd(provMatrix);
+    }
+
+    // Función que imprime el resultado de Floyd
+    // Complejidad temporal: O(n^2)
+    void printFloyd(vector<vector<int>> results)
+    {
+        cout << "Floyd:" << endl;
+        for (int i = 0; i < results.size(); i++)
+        {
+            for (int j = 0; j < results[i].size(); j++)
+            {
+                if (results[i][j] == INF)
+                {
+                    cout << "INF ";
+                }
+                else
+                {
+                    cout << results[i][j] << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
 };
-
-
--- A01639912_Act3.2/main.cpp
+++ A01639912_Act3.2/main.cpp
@@ -3,25 +3,31 @@
 #include "graph.h"
 using namespace std;
 
-void printMatrix(vector<vector<int>> matrizAdj){
-for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
-            cout<<matrizAdj[i][j]<<" ";
+void printMatrix(vector<vector<int>> matrizAdj)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << matrizAdj[i][j] << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
 }
 
-int main(){
-    int n = 0;
-    cin>>n;
+int main(int argc, char *argv[])
+{
+    int n;
+    cin >> n;
 
     vector<vector<int>> matrizAdj;
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         vector<int> row;
-        for(int j = 0; j < n; j++){
+        for (int j = 0; j < n; j++)
+        {
             int num = 0;
-            cin>>num;
+            cin >> num;
             row.push_back(num);
         }
         matrizAdj.push_back(row);
@@ -32,6 +38,7 @@ int main(){
     Graph grafo(matrizAdj);
 
     grafo.dijkstra(0);
-    
+    grafo.floyd(n);
+
     return 0;
 }
